18f1bf0c5f37f2b34f54f0f23961f4c9cac155ca
SuMit YaDav <94666370+thesumityadav21@users.noreply.github.com>
2024-01-21 16:05:10 +0530

    Create 21Jan.py
    if else

---
+++ 21Jan.py
@@ -0,0 +1,32 @@
+'''
+initialization - Starting  value
+while (condition): ending value
+statement
+increase/decrease
+
+i = 1
+while (i <= 10):
+    print(i)
+    i = i + 1
+'''
+
+'''
+Conditional Statement 
+
+if (Condition):
+    Statement
+
+if (condition):
+    statement 1
+else:
+    statement 2
+
+if (condition):
+    statement 1
+elif(condition 2):
+    statement 2
+elif(condition 3):
+    statement 3
+else:
+    statement ( Not Mandatory)
+'''
