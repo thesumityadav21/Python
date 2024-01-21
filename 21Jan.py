81e2404a55de8c9c0aaa212d5c11ec6ebd806f02
SuMit YaDav <94666370+thesumityadav21@users.noreply.github.com>
2024-01-21 16:23:51 +0530

    Update 21Jan.py

--- 21Jan.py
+++ 21Jan.py
@@ -1,4 +1,4 @@
-'''
+"""
 initialization - Starting  value
 while (condition): ending value
 statement
@@ -8,7 +8,7 @@ i = 1
 while (i <= 10):
     print(i)
     i = i + 1
-'''
+"""
 
 '''
 Conditional Statement 
@@ -29,4 +29,25 @@ elif(condition 3):
     statement 3
 else:
     statement ( Not Mandatory)
+'''
+
+# Write a Program to print natural numbers from 1 to 10 but if I reach 5 program should terminate
+"""
+for i in range(1, 10):
+    if i == 5:  # break terminates the loop
+        break
+    print(i)
+
+for i in range(1, 10):
+    if i == 5:  # skips the line 
+        continue
+    print(i)
+"""
+# User Input
+'''
+a = input('write your name')
+print(a)
+
+a = int(input('Write a Value'))
+print(a*5)
 '''
