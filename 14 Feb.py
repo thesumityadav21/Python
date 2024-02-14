ed18806293e7285d146423d78b708b4caa055d5b
SuMit YaDav <94666370+thesumityadav21@users.noreply.github.com>
2024-02-14 23:43:01 +0530

    list & list slicing

---
+++ 14 Feb.py
@@ -0,0 +1,26 @@
+# list
+
+# list_name = [Value1, Value2, Value3...]
+
+"""lst = [4, "Ram", 10.78]
+
+for i in lst1:
+    print(i)"""
+# calculate homogenous data only in list.
+# 'in' is a membership operator.
+
+# declare list data type
+lst1 = []
+lst = [5, 10, 15, 20]
+lst1 = lst
+
+# list Slicing 
+print(lst[1:3])
+print(lst[::-1])
+print(lst[2:])
+
+
+
+
+
+
